5718ce313258be6a006d37f8ef499f03ab252292
Hans Carreon <hans.carreon@smsupermalls.com>
2024-06-06 14:14:16 +0800

    feat: PRDV-123 add new counter

--- mobile_sample/lib/features/profile/presentation/profile_screen.dart
+++ mobile_sample/lib/features/profile/presentation/profile_screen.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   static const String routeName = '/profile-screen';
   const ProfileScreen({super.key});
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  int _counter2 = 0;
+
+  void decrementCounter() {
+    setState(() {
+      _counter2--;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.amber,
       body: Column(
         children: [
-           Text('this is profile screen'),
+          const Text('this is profile screen'),
+          Text('this is profile counter $_counter2'),
+          ElevatedButton(
+            onPressed: decrementCounter,
+            child: const Text('Press Me'),
+          )
         ],
       ),
     );
