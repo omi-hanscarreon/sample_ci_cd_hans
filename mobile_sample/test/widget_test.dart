b6294f300f2352cc90b6eb37b4c6e656247a3afe
Hans Carreon <hans.carreon@smsupermalls.com>
2024-06-06 13:44:11 +0800

    fix: remove problems

--- mobile_sample/test/widget_test.dart
+++ mobile_sample/test/widget_test.dart
@@ -9,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sample_ci_cd_hans/features/home/presentation/home_screen.dart';
 
-import 'package:sample_ci_cd_hans/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
